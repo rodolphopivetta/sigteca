83b25253e87f481b1b405717fbe358ed8b5f1712
Rodolpho Pivetta <rodolpho.pivetta@gmail.com>
2015-08-17 16:22:31 -0400

    settings configuration

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sigteca.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sigteca.settings.active")
 
     from django.core.management import execute_from_command_line
 
